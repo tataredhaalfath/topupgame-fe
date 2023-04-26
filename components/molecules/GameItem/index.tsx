3ea31c9562f3b963e1dfe6e62fc4b1d86a618b89
Redha <tataredhaalfath@gmail.com>
2023-04-26 19:33:06 +0700

    Refactor Featured Game

---
+++ components/molecules/GameItem/index.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface GameItemProps {
+  thumbnail: string;
+  title: string;
+  category: string;
+}
+
+export default function GameItem(props: GameItemProps) {
+  const { thumbnail, title, category } = props;
+  return (
+    <div className="featured-game-card position-relative">
+      <Link href="/detail">
+        <div className="blur-sharp">
+          <Image
+            className="thumbnail"
+            src={`/img/${thumbnail}.png`}
+            width={205}
+            height={270}
+            alt="thumbnail"
+          />
+        </div>
+        <div className="cover position-absolute bottom-0 m-32">
+          <div className="d-flex flex-column h-100 justify-content-between text-decoration-none">
+            <div className="game-icon mx-auto">
+              <Image
+                src="/icon/console.svg"
+                width={54}
+                height={36}
+                alt="console"
+              />
+            </div>
+            <div>
+              <p className="fw-semibold text-white text-xl m-0">{title}</p>
+              <p className="fw-light text-white m-0">{category}</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
